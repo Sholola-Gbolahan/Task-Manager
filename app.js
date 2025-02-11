6474611a1c052115c9f67c0ef2a05286eb8c1f43
Sholola-Gbolahan <shololagbolahan@gmail.com>
2025-02-11 11:25:05 +0100

    20. func. to handle 404 across the App

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const tasks = require("./routes/tasks")
 require("dotenv").config()
 const connectDB = require("./db/connect")
+const notFound = require("./middleware/not-found")
 
 // middleware
 app.use(express.json())
@@ -10,11 +11,7 @@ app.use(express.static("./public"))
 
 app.use("/api/v1/tasks", tasks)
 
-// app.get('/api/v1/tasks')     - get all the task
-// app.post('/api/v1/tasks')     - create a new task
-// app.get('/api/v1/tasks/:id')     - get single task
-// app.patch('/api/v1/tasks/:id')     - update task
-// app.delete('/api/v1/tasks/:id')   - Delete task
+app.use(notFound)
 const port = 3000
 
 const startApp = async () => {
